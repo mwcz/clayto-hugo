e43ef7bdb58020fb774491b54d33a912a10b25cd
Michael Clayton <mclayton@redhat.com>
2019-02-15 15:28:14 -0500

    fix new script post numbers

--- scripts/new.sh
+++ scripts/new.sh
@@ -3,6 +3,8 @@
 m="$(ls content/posts | grep -oP "^\d+" | sort -r | head -1)"
 let "n = ${m#0} + 1"
 
-hugo new "posts/$n - $1/index.md"
+POST_PATH="posts/$(printf %03d $n) - $1/index.md"
 
-vim content/posts/$n*/index.md
+hugo new "$POST_PATH"
+
+vim "content/$POST_PATH"
