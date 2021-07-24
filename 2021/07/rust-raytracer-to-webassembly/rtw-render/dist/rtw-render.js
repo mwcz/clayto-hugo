70bb39d607c59c370598cd909023a2e93a9a149c
mwcz <mwc@clayto.com>
2021-07-23 23:00:37 -0500

    generate Hugo site

--- 2021/07/rust-raytracer-to-webassembly/rtw-render/dist/rtw-render.js
+++ 2021/07/rust-raytracer-to-webassembly/rtw-render/dist/rtw-render.js
@@ -1,22 +1,87 @@
-var s=class extends HTMLElement{constructor(){super();this.attachShadow({mode:"open"}),this.active=!1,this.paused=!1,this.shadowRoot.innerHTML=`
-        <style>
-            meter {
-                width: 100%;
-            }
-        </style>
-        <label>
-            <meter optimum=0 low=800 high=1500 value=0 max=2500></meter>
-            <span id="label-text">&nbsp;</span>
-        </label>
-        `,this.labelText=this.shadowRoot.querySelector("#label-text"),this.meter=this.shadowRoot.querySelector("meter"),this.step=this.step.bind(this)}start(){this.paused?this.paused=!1:this.startTime=performance.now(),this.active=!0,requestAnimationFrame(this.step)}pause(){this.paused=!0}step(){this.active&&!this.paused&&(this._updateLabel(),requestAnimationFrame(this.step))}_updateLabel(){let e=performance.now()-this.startTime;this.setLabel(`${e.toFixed(1)}ms`),this.meter.value=e}stop(){this._updateLabel(),this.active=!1}setLabel(e){this.labelText.innerText=e}},h=s;customElements.define("rtw-timer",s);var r=class extends HTMLElement{constructor(){super();this.attachShadow({mode:"open"}),fetch(`${import.meta.url}/../wasm_bg.wasm`),this.shadowRoot.innerHTML=`
+var p=class extends HTMLElement{constructor(){super();this.attachShadow({mode:"open"})}play(){this.style.setProperty("--play-state","running")}pause(){this.style.setProperty("--play-state","paused")}connectedCallback(){let o=this.hasAttribute("paused"),t=+this.getAttribute("box-count")||4,a=+this.getAttribute("duration")||Math.sqrt(t/4),e=new Array(t).fill(),s=a/(t*.7),n=100*s,d=`
+            content: "";
+            position: absolute;
+            display: block;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            border: var(--pbp-loading-border, 1px solid white);
+            border-width: $WIDTH;
+            animation: $DIM ${a}s backwards alternate infinite;
+            animation-delay: var(--delay);
+            animation-play-state: var(--play-state);
+        `;this.shadowRoot.innerHTML=`
             <style>
                 :host {
                     display: inline-block;
-                    background-color: var(--rtw-background-color, grey);
+                    --play-state: ${o?"paused":"running"};
+                }
+                div {
+                    display: grid;
+                    grid-template-columns: ${e.map(()=>"1fr").join(" ")};
+                    grid-gap: var(--pbp-loading-gap, 8px);
+                }
+                span {
+                    display: inline-block;
+                    position: relative;
+                    height: var(--pbp-loading-box-size, 20px);
+                    width: var(--pbp-loading-box-size, 20px);
+                }
+                @keyframes x {
+                  0% {
+                      transform: scaleX(var(--pbp-loading-grow, 150%));
+                      opacity: 0;
+                  }
+                  ${n}% {
+                      transform: scaleX(100%);
+                      opacity: 1;
+                  }
+                }
+                @keyframes y {
+                  0% {
+                      transform: scaleY(var(--pbp-loading-grow, 150%));
+                      opacity: 0;
+                  }
+                  ${n}% {
+                      transform: scaleY(100%);
+                      opacity: 1;
+                  }
+                }
+                span::before {
+                    ${d.replace("$WIDTH","1px 0 1px 0").replace("$DIM","y")}
+                }
+                span::after {
+                    ${d.replace("$WIDTH","0 1px 0 1px").replace("$DIM","x")}
+                }
+                */
+            </style>
+            <div>
+                ${e.map((c,l)=>`<span style="--delay:${l*s}s"></span>`).join("")}
+            </div>
+        `}};customElements.define("pbp-loading",p);var r=class extends HTMLElement{constructor(){super();this.attachShadow({mode:"open"}),this.active=!1,this.paused=!1,this.step=this.step.bind(this)}connectedCallback(){this.shadowRoot.innerHTML=`
+        <style>
+            pbp-loading {
+                width: 100%;
+            }
+            label {
+                display: grid;
+                grid-template-columns: 1fr;
+                grid-gap: 6px;
+            }
+        </style>
+        <label>
+            <pbp-loading paused duration=0.8 box-count=8></pbp-loading>
+            <span id="label-text">&nbsp;</span>
+        </label>
+        `,this.labelText=this.shadowRoot.querySelector("#label-text"),this.loading=this.shadowRoot.querySelector("pbp-loading"),console.log("loading",this.loading)}resetSpinner(){this.loading.style.removeProperty("--play-state"),this.labelText.parentNode.removeChild(this.loading),this.labelText.parentNode.insertBefore(this.loading,this.labelText)}start(){this.paused?this.paused=!1:this.startTime=performance.now(),this.active=!0,this.loading.play(),requestAnimationFrame(this.step)}pause(){this.paused=!0,this.loading.pause()}step(){this.active&&!this.paused&&(this._updateLabel(),requestAnimationFrame(this.step))}_updateLabel(){let t=performance.now()-this.startTime;this.setLabel(`${t.toFixed(1)}ms`)}stop(){this.resetSpinner(),this._updateLabel(),this.active=!1}setLabel(t){this.labelText.innerText=t}},g=r;customElements.define("rtw-timer",r);var i=class extends HTMLElement{constructor(){super();this.attachShadow({mode:"open"}),fetch(`${import.meta.url}/../wasm_bg.wasm`),this.shadowRoot.innerHTML=`
+            <style>
+                :host {
+                    display: inline-block;
+                    background-color: var(--rtw-background-color, black);
                     padding: 14px;
                 }
                 canvas {
-                    aspect-ratio: 5/3.33;
                     width: 100%;
                     image-rendering: -moz-crisp-edges;
                     image-rendering: -webkit-crisp-edges;
@@ -29,7 +94,17 @@ var s=class extends HTMLElement{constructor(){super();this.attachShadow({mode:"o
                     display: grid;
                     grid-template-columns: 1fr 1fr;
                     grid-gap: 14px;
-                    overflow-y: hidden;
+                }
+                button {
+                    border: var(--rtw-button-border, 1px solid white);
+                    background: var(--rtw-button-background, black);
+                    color: var(--rtw-button-color, white);
+                }
+                button:hover {
+                    background: var(--rtw-button-background-hover, #1f1f1f);
+                }
+                button:active, button:focus {
+                    background: var(--rtw-button-background-active, #3f3f3f);
                 }
             </style>
 
@@ -38,4 +113,4 @@ var s=class extends HTMLElement{constructor(){super();this.attachShadow({mode:"o
                 <button disabled>Render</button>
                 <rtw-timer></rtw-timer>
             </div>
-        `}connectedCallback(){this.btn=this.shadowRoot.querySelector("button"),this.canvas=this.shadowRoot.querySelector("canvas"),this.timer=this.shadowRoot.querySelector("rtw-timer"),this.ctx=this.canvas.getContext("2d"),this.moduleWorkerSupported=!0,this.worker=this.createWorker(),this.wasmInit=null,this.wasmRender=null,this.btn.addEventListener("click",()=>{this.preRender(),this.render()})}createWorker(){let e=new URL(`${import.meta.url}/../wasm-worker.js`),a=new Worker(e.href,{type:"module"});return a.addEventListener("message",async t=>{if(t.data.status==="success")t.data.data.imageData?this.postRender(t.data.data.imageData):t.data.data.initialized&&(this.btn.disabled=!1);else if(t.data.status==="error"){if(console.log(`web worker error type: ${t.data.data.type}`),t.data.data.type==="import"){this.moduleWorkerSupported=!1,this.btn.disabled=!1;let i=await import("./wasm-render.js");this.wasmInit=i.wasmInit,this.wasmRender=i.wasmRender,await wasmInit(),this.timer.pause(),this.timer.setLabel("Module worker not supported in this browser; running on the main thread (expect lockup during render).")}t.data.data.type==="render"&&(this.timer.pause(),this.timer.setLabel("Error occurred in worker during rendering."))}}),a.postMessage("init"),a}async preRender(){this.timer.start(),this.btn.disabled=!0}async render(){console.log(`starting render ${["ON","OFF"][~~this.moduleWorkerSupported]} the main thread`),this.moduleWorkerSupported?this.worker.postMessage("render"):this.postRender(await this.wasmRender())}postRender(e){console.time("drawing canvas"),this.ctx.putImageData(e,0,0),console.timeEnd("drawing canvas"),this.timer.step(),this.timer.stop(),this.btn.innerText="Re-render",this.btn.disabled=!1}},n=r;customElements.define("rtw-render",r);export{n as default};
+        `}connectedCallback(){this.btn=this.shadowRoot.querySelector("button"),this.canvas=this.shadowRoot.querySelector("canvas"),this.timer=this.shadowRoot.querySelector("rtw-timer"),this.ctx=this.canvas.getContext("2d"),this.moduleWorkerSupported=!0,this.worker=this.createWorker(),this.wasmInit=null,this.wasmRender=null,this.btn.addEventListener("click",()=>{this.preRender(),this.render()})}createWorker(){let t=new URL(`${import.meta.url}/../wasm-worker.js`),a=new Worker(t.href,{type:"module"});return a.addEventListener("message",async e=>{if(e.data.status==="success")e.data.data.imageData?this.postRender(e.data.data.imageData):e.data.data.initialized&&(this.btn.disabled=!1);else if(e.data.status==="error"){if(console.log(`web worker error type: ${e.data.data.type}`),e.data.data.type==="import"){this.moduleWorkerSupported=!1,this.btn.disabled=!1;let s=await import("./wasm-render.js");this.wasmInit=s.wasmInit,this.wasmRender=s.wasmRender,await wasmInit(),this.timer.pause(),this.timer.setLabel("Module worker not supported in this browser; running on the main thread (expect lockup during render).")}e.data.data.type==="render"&&(this.timer.pause(),this.timer.setLabel("Error occurred in worker during rendering."))}}),a.postMessage("init"),a}async preRender(){this.timer.start(),this.btn.disabled=!0}async render(){console.log(`starting render ${["ON","OFF"][~~this.moduleWorkerSupported]} the main thread`),this.moduleWorkerSupported?this.worker.postMessage("render"):this.postRender(await this.wasmRender())}postRender(t){console.time("drawing canvas"),this.ctx.putImageData(t,0,0),console.timeEnd("drawing canvas"),this.timer.step(),this.timer.stop(),this.btn.innerText="Re-render",this.btn.disabled=!1}},h=i;customElements.define("rtw-render",i);export{h as default};
